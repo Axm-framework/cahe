81c6d9dce9061227a55cca033e1770f4f57bfc96
Juan Cristobal <juancristobalgd1@gmail.com>
2023-07-14 22:13:03 +0200

    Changes to be committed: 	modified:   src/Cache.php 	deleted:    src/Drivers/APCCache.php 	modified:   src/Drivers/FileCache.php 	deleted:    src/Drivers/NixfileCache.php 	deleted:    src/Drivers/SqliteCache.php 	modified:   src/InterfaceCache.php

--- src/Cache.php
+++ src/Cache.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace Axm\Cache;
@@ -41,47 +40,3 @@ abstract class Cache implements InterfaceCache
         self::$_default_driver = $driver;
     }
 }
-=======
-<?php
-
-namespace Axm\Cache;
-
-use Axm\Cache\InterfaceCache;
-
-abstract class Cache implements InterfaceCache
-{
-    protected static $_drivers = [];
-    protected static $_default_driver = 'File';
-    protected static $_namespace = 'Axm\\Cache\\Drivers\\';
-    protected $_id;
-    protected $_group = 'default';
-    protected $_lifetime = '';
-    protected $_start = [];
-
-    /**
-     * 
-     */
-    public static function driver($driver = null)
-    {
-        if (empty($driver)) {
-            $driver = self::$_default_driver;
-        }
-
-        if (!array_key_exists($driver, self::$_drivers)) {
-            $class = $driver . 'Cache';
-            $classNamespace = static::$_namespace . $class;
-            self::$_drivers[$driver] = new $classNamespace();
-        }
-
-        self::$_drivers[$driver]->init();
-
-        return self::$_drivers[$driver];
-    }
-
-
-    public static function setDefaultDriver($driver = 'File')
-    {
-        self::$_default_driver = $driver;
-    }
-}
->>>>>>> 3259b0c57a0b29618482e92c8c88a75bdb50ecfb
